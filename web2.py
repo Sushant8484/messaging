49192346027afb5608c53bebc002a431cd9cbcd6
Sushant8484 <78917443+Sushant8484@users.noreply.github.com>
2023-02-07 16:32:27 +0530

    Add files via upload

---
+++ web2.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
+app.config['DEBUG'] = True
+socketio = SocketIO(app)
+
+users = {}
+
+@app.route('/')
+def index():
+    return render_template('message.html')
+
+@socketio.on('username', namespace='/private')
+def receive_username(username):
+    users[username] = request.sid
+    #users.append({username : request.sid})
+    #print(users)
+    print('Username added!')
+    print(users)
+
+@socketio.on('private_message', namespace='/private')
+def private_message(payload):
+    recipient_session_id = users[payload['username']]
+    message = payload['message']
+
+    emit('new_private_message', message, room=recipient_session_id)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
